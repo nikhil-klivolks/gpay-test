5adf2409a839feff8ed6d6a2248f57a0f60cf17a
Nikhil D Anand <nikhildanand@gmail.com>
2023-02-28 20:28:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import GooglePayButton from "@google-pay/button-react";
+
+export default function App() {
+  const paymentRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: "CARD",
+        parameters: {
+          allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+          allowedCardNetworks: ["MASTERCARD", "VISA"],
+        },
+        tokenizationSpecification: {
+          type: "DIRECT",
+          parameters: {
+            protocolVersion: "ECv2",
+            publicKey: import.meta.env.VITE_APP_PUBLIC_KEY,
+          },
+        },
+      },
+    ],
+    merchantInfo: {
+      merchantId: import.meta.env.VITE_APP_MERCHANT_ID,
+      merchantName: import.meta.env.VITE_APP_MERCHANT_NAME,
+    },
+    transactionInfo: {
+      totalPriceStatus: "FINAL",
+      totalPriceLabel: "Total",
+      totalPrice: "1.00",
+      currencyCode: "INR",
+      countryCode: "IN",
+    },
+  };
+
+  const isTop = window === window.top;
+
+  return (
+    <div>
+      <div className="demo">
+        <GooglePayButton
+          environment="TEST"
+          buttonType="plain"
+          buttonSizeMode="fill"
+          paymentRequest={paymentRequest}
+          onLoadPaymentData={(paymentRequest) => {
+            console.log("load payment data", paymentRequest);
+          }}
+          onError={(error) => {
+            if (error instanceof Error) {
+              console.log("Error", error, error.message, error.stack);
+            } else {
+              console.log("Error", error.statusCode, error.statusMessage);
+            }
+          }}
+        />
+      </div>
+
+      <div className="note" style={{ display: isTop ? "none" : "" }}>
+        <p>
+          Note: This page may need to open in a new window for it to function
+          correctly.
+        </p>
+        <p>
+          <a href="/" target="_blank">
+            Open in new window
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
+}
